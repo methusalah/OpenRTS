165b7cb6b04a8f23932b6d13bd8696cdea72b874
Benoît <ben.oeilnoir@gmail.com>
2014-12-28 00:16:10 +0100

    fix #41, operations du height tool. le height tool peut désormais appliquer du bruit, adoucir, ou aplatir

--- RTSForGit/src/model/map/editor/tools/HeightTool.java
+++ RTSForGit/src/model/map/editor/tools/HeightTool.java
@@ -5,6 +5,7 @@
 package model.map.editor.tools;
 
 import java.util.ArrayList;
+import math.MyRandom;
 import model.map.Tile;
 import model.map.editor.MapToolManager;
 import model.map.editor.Pencil;
@@ -15,8 +16,12 @@ import tools.LogUtil;
  * @author Benoît
  */
 public class HeightTool extends MapTool {
+    enum Operation {RaiseLow, NoiseSmooth, Uniform}
+    
+    Operation actualOp = Operation.RaiseLow;
 
     double amplitude = 0.2;
+    double maintainedElevation;
     
     public HeightTool(MapToolManager manager, Pencil selector) {
         super(manager, selector);
@@ -25,17 +30,69 @@ public class HeightTool extends MapTool {
     @Override
     public void primaryAction() {
         ArrayList<Tile> group = pencil.getTiles();
-        for(Tile t : group)
-            t.elevation+=amplitude*pencil.getApplicationRatio(t.getPos2D());
+        switch (actualOp){
+            case RaiseLow : raise(group); break;
+            case NoiseSmooth : noise(group); break;
+            case Uniform : uniform(group); break;
+        }
         manager.updateParcels(group);
     }
 
     @Override
     public void secondaryAction() {
         ArrayList<Tile> group = pencil.getTiles();
-        for(Tile t : group)
-            t.elevation-=amplitude*pencil.getApplicationRatio(t.getPos2D());
+        switch (actualOp){
+            case RaiseLow : low(group); break;
+            case NoiseSmooth : smooth(group); break;
+            case Uniform : break;
+        }
         manager.updateParcels(group);
+    }
+    
+    private void raise(ArrayList<Tile> tiles){
+        for(Tile t : tiles)
+            t.elevation += amplitude*pencil.getApplicationRatio(t.getPos2D());
+    }
+    
+    private void low(ArrayList<Tile> tiles){
+        for(Tile t : tiles)
+            t.elevation -= amplitude*pencil.getApplicationRatio(t.getPos2D());
+    }
+    
+    private void uniform(ArrayList<Tile> tiles){
+        if(!pencil.maintained){
+            pencil.maintain();
+            maintainedElevation = manager.map.getGroundAltitude(pencil.getPos());
+        }
+        for(Tile t : tiles){
+            double diff = maintainedElevation-t.elevation;
+            double attenuatedAmplitude = amplitude*pencil.getApplicationRatio(t.getPos2D());
+            if(diff > 0)
+                t.elevation += Math.min(diff, attenuatedAmplitude);
+            else if(diff < 0)
+                t.elevation += Math.max(diff, -attenuatedAmplitude);
+        }
+    }
+    private void noise(ArrayList<Tile> tiles){
+        for(Tile t : tiles){
+            t.elevation += amplitude*MyRandom.between(-1.0, 1.0)*pencil.getApplicationRatio(t.getPos2D());
+        }
+    }
+
+    private void smooth(ArrayList<Tile> tiles){
+        double average = 0;
+        for(Tile t : tiles)
+            average += t.elevation;
+        average /= tiles.size();
+        
+        for(Tile t : tiles){
+            double diff = average-t.elevation;
+            double attenuatedAmplitude = amplitude*pencil.getApplicationRatio(t.getPos2D());
+            if(diff > 0)
+                t.elevation += Math.min(diff, attenuatedAmplitude);
+            else if(diff < 0)
+                t.elevation += Math.max(diff, -attenuatedAmplitude);
+        }
     }
 
     @Override
@@ -45,7 +102,20 @@ public class HeightTool extends MapTool {
 
     @Override
     public void toggleOperation() {
-        LogUtil.logger.info("Height tool has no other operation for now.");
+        switch (actualOp){
+            case RaiseLow :
+                actualOp = Operation.NoiseSmooth;
+                LogUtil.logger.info("Atlas tool operation toggled to noise/smooth.");
+                break;
+            case NoiseSmooth :
+                actualOp = Operation.Uniform;
+                LogUtil.logger.info("Atlas tool operation toggled to uniform.");
+                break;
+            case Uniform :
+                actualOp = Operation.RaiseLow;
+                LogUtil.logger.info("Atlas tool operation toggled to raise/low.");
+                break;
+        }
     }
     
     
