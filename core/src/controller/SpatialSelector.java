e3607c56569dc4c86c9580b4debd956f72f73c6a
Mario Meltzow <github@meltzow.net>
2015-05-16 15:39:03 +0200

    Code QA:

--- core/src/controller/SpatialSelector.java
+++ core/src/controller/SpatialSelector.java
@@ -19,69 +19,71 @@ public class SpatialSelector {
 	InputManager im;
 	View view;
 	public boolean centered = false;
-	
+
 	public SpatialSelector(Camera c, InputManager im, View v) {
 		cam = c;
 		this.im = im;
 		view = v;
 	}
-	
+
 	public Geometry getGeometry(Node n) {
 		Ray r;
-		if(centered)
+		if(centered) {
 			r = getCameraRay();
-		else 
+		} else {
 			r = getMouseRay();
-                return view.pointer.getPointedGeometry(n, r);
+		}
+		return view.pointer.getPointedGeometry(n, r);
 	}
-	
+
 	public Point2D getCoord(Node n) {
 		Ray r;
-		if(centered)
+		if(centered) {
 			r = getCameraRay();
-		else 
+		} else {
 			r = getMouseRay();
-        return view.pointer.getPointedCoord(n, r);
+		}
+		return view.pointer.getPointedCoord(n, r);
 	}
-	
+
 	public Point2D getCenterViewCoord(Node n) {
-        return view.pointer.getPointedCoord(n, getCameraRay());
+		return view.pointer.getPointedCoord(n, getCameraRay());
 	}
-	
+
 	public Point2D getCoord(Node n, Point2D screenCoord) {
 		Vector3f origin = cam.getWorldCoordinates(Translator.toVector2f(screenCoord), 0f);
 		Vector3f direction = cam.getWorldCoordinates(Translator.toVector2f(screenCoord), 1f);
 		direction.subtractLocal(origin).normalizeLocal();
 		Ray r = new Ray(origin, direction);
-        return view.pointer.getPointedCoord(n, r);
+		return view.pointer.getPointedCoord(n, r);
 	}
-	
+
 	public Point2D getScreenCoord(Point3D pos){
-    	Vector3f vPos = Translator.toVector3f(pos);
-    	Vector3f screenCoord = cam.getScreenCoordinates(vPos);
-    	return Translator.toPoint3D(screenCoord).get2D();
+		Vector3f vPos = Translator.toVector3f(pos);
+		Vector3f screenCoord = cam.getScreenCoordinates(vPos);
+		return Translator.toPoint3D(screenCoord).get2D();
 	}
-	
+
 	private Ray getMouseRay(){
 		Vector3f origin = cam.getWorldCoordinates(im.getCursorPosition(), 0f);
 		Vector3f direction = cam.getWorldCoordinates(im.getCursorPosition(), 1f);
 		direction.subtractLocal(origin).normalizeLocal();
 		return new Ray(origin, direction);
 	}
-	
+
 	private Ray getCameraRay(){
 		return new Ray(cam.getLocation(), cam.getDirection());
 	}
-        
-    public String getSpatialLabel(){
-        Spatial s = getGeometry(view.rootNode);
-        while(true){
-            if(s == null || s.getName() == null)
-                return null;
-            if(s.getName().startsWith("label"))
-                return s.getName();
-            s = s.getParent();
-        }
-    }
+
+	public String getSpatialLabel(){
+		Spatial s = getGeometry(view.rootNode);
+		while (s != null && s.getName() != null) {
+			if(s.getName().startsWith("label")) {
+				return s.getName();
+			}
+			s = s.getParent();
+		}
+		return null;
+	}
 
 }
