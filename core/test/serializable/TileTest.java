23972707954ec77f25891bbeb7323ae0b3f91224
Mario Meltzow <github@meltzow.net>
2015-06-01 21:52:47 +0200

    forgot test

---
+++ core/test/serializable/TileTest.java
@@ -0,0 +1,74 @@
+package serializable;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import model.battlefield.map.Map;
+import model.battlefield.map.Tile;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.smile.SmileFactory;
+
+/**
+ *
+ * @author wuendsch
+ */
+public class TileTest {
+
+	@Test
+	public void binaryTiles() throws IOException {
+
+		Map m = new Map();
+		m.tiles.add(new Tile(20, 20, 1, 30.4, "test"));
+
+		SmileFactory f = new SmileFactory();
+		// can configure instance with 'SmileParser.Feature' and 'SmileGenerator.Feature'
+		ObjectMapper mapper = new ObjectMapper(f);
+		// and then read/write data as usual
+
+		byte[] smileData = mapper.writeValueAsBytes(m.tiles);
+		OutputStream out = new FileOutputStream("tiles.openrts");
+		out.write(smileData);
+		out.flush();
+		out.close();
+
+		// InputStream in = new FileInputStream("tiles.openrts");
+		// in.read(smileData);
+
+		List otherValue = mapper.readValue(smileData, List.class);
+
+
+		// OpenRTSClient client = new OpenRTSClient();
+		// client.start();
+
+		// assert ((new LifeLeechComponent()) instanceof Component);
+		// assert e.hasComponent(LifeLeechComponent.class);
+		//        assert false;
+
+
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@BeforeMethod
+	public void setUpMethod() throws Exception {
+	}
+
+	@AfterMethod
+	public void tearDownMethod() throws Exception {
+	}
+}
