cfcfb2f36687449d5323c5db48741ec1accdccd9
Mario Meltzow <github@meltzow.net>
2015-07-05 10:02:50 +0200

    waiting untill server sends response

--- server/test/java/openrts/server/ServerTest.java
+++ server/test/java/openrts/server/ServerTest.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+import network.client.ClientManager;
+
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -12,13 +15,17 @@ import org.testng.annotations.Test;
 
 import com.jme3.system.JmeContext;
 
+import controller.game.MultiplayerGameInputInterpreter;
+import event.EventManager;
+import event.InputEvent;
+import event.ServerEvent;
+import event.WorldChangedEvent;
+import geometry.geom2d.Point2D;
+
 /**
- * @author wuendsch
+ * @author mario
  */
 public class ServerTest {
-
-	public ServerTest() {
-	}
 
 	@Test
 	public void testInput() throws Exception {
@@ -26,50 +33,60 @@ public class ServerTest {
 		OpenRTSServer app = new OpenRTSServer();
 		app.start(JmeContext.Type.Headless);
 
-		// startSecondJVM(OpenRTSServer.class, true);
+		waitUntilServerIsStarted();
 
-		boolean scanning = true;
-		while (scanning) {
-			Socket socket = new Socket();
-			InetSocketAddress sa = new InetSocketAddress("localhost", OpenRTSServer.PORT);
-			try {
-				socket.connect(sa, 500);
-				boolean connected = true;
-				scanning = false;
-			} catch (IOException e) {
-				System.out.println("Connect failed, waiting and trying again");
-			}
-			try {
-				Thread.sleep(2000);// 2 seconds
-			} catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-			socket.close();
-		}
+		ClientEventListenerMock obj = new ClientEventListenerMock();
+		EventManager.registerForClient(obj);
 
-		//
-		// ClientEventListenerMock obj = new ClientEventListenerMock();
-		// EventManager.registerForClient(obj);
-		//
-		// ClientManager.startClient();
-		// InputEvent evt = new InputEvent(MultiplayerGameInputInterpreter.SELECT, new Point2D(1, 1), true);
-		// ClientManager.getInstance().manageEvent(evt);
-		//
-		// ServerEvent ev = obj.getEvent();
-		// Assert.assertTrue(ev instanceof WorldChangedEvent);
+		ClientManager.startClient();
+		InputEvent evt = new InputEvent(MultiplayerGameInputInterpreter.SELECT, new Point2D(1, 1), true);
+		ClientManager.getInstance().manageEvent(evt);
+
+		waitUntilClientHasResponse(obj);
+		ServerEvent ev = obj.getEvent();
+		Assert.assertTrue(ev instanceof WorldChangedEvent);
 
 	}
 
-	private void startSecondJVM(Class<? extends Object> clazz, boolean redirectStream) throws Exception {
-		System.out.println(clazz.getCanonicalName());
-		String separator = System.getProperty("file.separator");
-		String classpath = System.getProperty("java.class.path");
-		String path = System.getProperty("java.home") + separator + "bin" + separator + "java";
-		ProcessBuilder processBuilder = new ProcessBuilder(path, "-cp", classpath, clazz.getCanonicalName());
-		processBuilder.redirectErrorStream(redirectStream);
-		Process process = processBuilder.start();
-		process.waitFor();
-		System.out.println("Fin");
+	private void waitUntilClientHasResponse(ClientEventListenerMock mock) throws IOException {
+		int waitingCounter = 0;
+		boolean waiting = true;
+		while (waiting) {
+			waitingCounter++;
+			if (mock.getEvent() != null) {
+				waiting = false;
+			}
+			if (waitingCounter > 10) {
+				Assert.fail("Client are waiting too long..");
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
+	}
+
+	private void waitUntilServerIsStarted() throws IOException {
+		boolean scanning = true;
+		while (scanning) {
+			Socket socket = new Socket();
+			try {
+				InetSocketAddress sa = new InetSocketAddress("localhost", OpenRTSServer.PORT);
+
+				socket.connect(sa, 500);
+				scanning = false;
+			} catch (IOException e) {
+				System.out.println("Connect failed, waiting and trying again");
+			} finally {
+				socket.close();
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
 	}
 
 	@BeforeClass
