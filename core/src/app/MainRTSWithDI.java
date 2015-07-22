a72b327cd518f5a374a3ded45290315b7db59043
Mario Meltzow <github@meltzow.net>
2015-07-22 21:02:43 +0200

    minimal invasive Dependencies Injections.

---
+++ core/src/app/MainRTSWithDI.java
@@ -0,0 +1,111 @@
+package app;
+
+import java.util.logging.Logger;
+
+import model.ModelManager;
+import model.editor.ToolManager;
+import view.EditorView;
+import view.mapDrawing.MapDrawer;
+
+import com.google.common.eventbus.Subscribe;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.math.Vector3f;
+import com.jme3.niftygui.NiftyJmeDisplay;
+
+import controller.Controller;
+import controller.battlefield.BattlefieldController;
+import controller.editor.EditorController;
+import controller.ground.GroundController;
+import event.EventManager;
+import event.client.ControllerChangeEvent;
+
+public class MainRTSWithDI extends OpenRTSApplicationWithDI {
+
+	private static final Logger logger = Logger.getLogger(MainRTSWithDI.class.getName());
+
+	EditorView view;
+	MapDrawer tr;
+	BattlefieldController fieldCtrl;
+	EditorController editorCtrl;
+	GroundController groundCtrl;
+	Controller actualCtrl;
+
+	public static void main(String[] args) {
+		OpenRTSApplicationWithDI.main(new MainRTSWithDI());
+	}
+
+	@Override
+	public void guiceAppInit() {
+		bulletAppState = new BulletAppState();
+		stateManager.attach(bulletAppState);
+		bulletAppState.getPhysicsSpace().setGravity(new Vector3f(0, 0, -1));
+		// stateManager.detach(bulletAppState);
+
+		flyCam.setUpVector(new Vector3f(0, 0, 1));
+		flyCam.setEnabled(false);
+
+		view = new EditorView(rootNode, guiNode, bulletAppState.getPhysicsSpace(), assetManager, viewPort);
+
+		NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, audioRenderer, guiViewPort);
+
+		fieldCtrl = new BattlefieldController(view, niftyDisplay.getNifty(), inputManager, cam);
+		editorCtrl = new EditorController(view, niftyDisplay.getNifty(), inputManager, cam);
+		groundCtrl = new GroundController(view, niftyDisplay.getNifty(), inputManager, cam);
+		EventManager.register(this);
+
+		niftyDisplay.getNifty().setIgnoreKeyboardEvents(true);
+		// TODO: validation is needed to be sure everyting in XML is fine. see http://wiki.jmonkeyengine.org/doku.php/jme3:advanced:nifty_gui_best_practices
+		// niftyDisplay.getNifty().validateXml("interface/screen.xml");
+		niftyDisplay.getNifty().fromXml("interface/screen.xml", "editor");
+
+		actualCtrl = editorCtrl;
+		stateManager.attach(actualCtrl);
+		actualCtrl.setEnabled(true);
+
+		guiViewPort.addProcessor(niftyDisplay);
+
+		ModelManager.setNewBattlefield();
+	}
+
+	@Override
+	public void simpleUpdate(float tpf) {
+		float maxedTPF = Math.min(tpf, 0.1f);
+		listener.setLocation(cam.getLocation());
+		listener.setRotation(cam.getRotation());
+		view.getActorManager().render();
+		actualCtrl.update(maxedTPF);
+		ModelManager.updateConfigs();
+	}
+
+	@Override
+	public void destroy() {
+		ToolManager.killSower();
+	}
+
+	@Subscribe
+	public void handleEvent(ControllerChangeEvent e) {
+		Controller desiredCtrl;
+		switch (e.getControllerIndex()) {
+			case 0:
+				desiredCtrl = fieldCtrl;
+				break;
+			case 1:
+				desiredCtrl = editorCtrl;
+				break;
+			case 2:
+				desiredCtrl = groundCtrl;
+				break;
+			default:
+				return;
+		}
+		logger.info("switching controller to " + desiredCtrl.getClass().getSimpleName());
+
+		stateManager.detach(actualCtrl);
+		actualCtrl.setEnabled(false);
+		actualCtrl = desiredCtrl;
+		stateManager.attach(actualCtrl);
+		actualCtrl.setEnabled(true);
+
+	}
+
+}
---
+++ core/src/app/OpenRTSApplicationWithDI.java
@@ -0,0 +1,272 @@
+package app;
+
+import exception.TechnicalException;
+import geometry.math.RandomUtil;
+
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.util.logging.Logger;
+import java.util.prefs.BackingStoreException;
+
+import openrts.guice.GuiceApplication;
+
+import com.jme3.app.StatsView;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.PhysicsTickListener;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial.CullHint;
+import com.jme3.system.AppSettings;
+import com.jme3.system.JmeContext.Type;
+import com.jme3.system.JmeSystem;
+import com.jme3.util.BufferUtils;
+
+public abstract class OpenRTSApplicationWithDI extends GuiceApplication implements PhysicsTickListener {
+
+	private static final Logger logger = Logger.getLogger(OpenRTSApplicationWithDI.class.getName());
+
+	public static OpenRTSApplicationWithDI appInstance;
+
+	protected Node rootNode = new Node("Root Node");
+	protected Node guiNode = new Node("Gui Node");
+
+	protected float secondCounter = 0.0f;
+
+	protected BitmapText fpsText;
+	protected MyDebugger debugger;
+	protected StatsView statsView;
+
+	// protected AzertyFlyByCamera flyCam;
+
+	private AppActionListener actionListener = new AppActionListener();
+
+	protected BulletAppState bulletAppState;
+
+	private class AppActionListener implements ActionListener {
+		@Override
+		public void onAction(String name, boolean value, float tpf) {
+			if (!value) {
+				return;
+			}
+
+			if (name.equals("SIMPLEAPP_Exit")) {
+				stop();
+			} else if (name.equals("SIMPLEAPP_CameraPos")) {
+				if (cam != null) {
+					Vector3f loc = cam.getLocation();
+					Quaternion rot = cam.getRotation();
+					System.out.println("Camera Position: (" + loc.x + ", " + loc.y + ", " + loc.z + ")");
+					System.out.println("Camera Rotation: " + rot);
+					System.out.println("Camera Direction: " + cam.getDirection());
+				}
+			} else if (name.equals("SIMPLEAPP_Memory")) {
+				BufferUtils.printCurrentDirectMemory(null);
+			}
+		}
+	}
+
+	@Override
+	public void start() {
+		super.start();
+		// set some default settings in-case
+		// settings dialog is not shown
+		if (settings == null) {
+			setSettings(new AppSettings(true));
+			settings.setWidth(1024);
+			settings.setHeight(768);
+			// try {
+			// settings.load("openrts.example");
+			// } catch (BackingStoreException e) {
+			// // TODO Auto-generated catch block
+			// e.printStackTrace();
+			// }
+		}
+
+	}
+
+
+	// @Override
+	// public AzertyFlyByCamera getFlyByCamera() {
+	// return flyCam;
+	// }
+
+	@Override
+	public Node getGuiNode() {
+		return guiNode;
+	}
+
+
+	@Override
+	public Node getRootNode() {
+		return rootNode;
+	}
+
+	private void initTexts() {
+		BitmapFont guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+		fpsText = new BitmapText(guiFont, false);
+		fpsText.setLocalTranslation(0, fpsText.getLineHeight(), 0);
+		fpsText.setText("Frames per second");
+		guiNode.attachChild(fpsText);
+
+		debugger = new MyDebugger(0, settings.getHeight(), assetManager.loadFont("Interface/Fonts/Console.fnt"));
+		guiNode.attachChild(debugger.getNode());
+	}
+
+	public void loadStatsView() {
+		statsView = new StatsView("Statistics View", assetManager, renderer.getStatistics());
+		// move it up so it appears above fps text
+		statsView.setLocalTranslation(0, fpsText.getLineHeight(), 0);
+		guiNode.attachChild(statsView);
+	}
+
+	@Override
+	public void initialize() {
+		bulletAppState = new BulletAppState();
+		bulletAppState.startPhysics();
+
+		super.initialize();
+
+		guiNode.setQueueBucket(Bucket.Gui);
+		guiNode.setCullHint(CullHint.Never);
+		initTexts();
+		loadStatsView();
+		viewPort.attachScene(rootNode);
+		guiViewPort.attachScene(guiNode);
+
+		if (inputManager != null) {
+			flyCam = new AzertyFlyByCamera(cam);
+			flyCam.setMoveSpeed(1f);
+			flyCam.registerWithInput(inputManager);
+
+			if (context.getType() == Type.Display) {
+				inputManager.addMapping("SIMPLEAPP_Exit", new KeyTrigger(KeyInput.KEY_ESCAPE));
+			}
+
+			inputManager.addMapping("SIMPLEAPP_CameraPos", new KeyTrigger(KeyInput.KEY_C));
+			inputManager.addMapping("SIMPLEAPP_Memory", new KeyTrigger(KeyInput.KEY_M));
+			inputManager.addListener(actionListener, "SIMPLEAPP_Exit", "SIMPLEAPP_CameraPos", "SIMPLEAPP_Memory");
+		}
+
+		// call user code
+		simpleInitApp();
+		stateManager.attach(bulletAppState);
+		getPhysicsSpace().addTickListener(this);
+	}
+
+	@Override
+	public void update() {
+		super.update(); // makes sure to execute AppTasks
+		if (speed == 0 || paused) {
+			return;
+		}
+
+		float tpf = timer.getTimePerFrame() * speed;
+
+		secondCounter += timer.getTimePerFrame();
+		int fps = (int) timer.getFrameRate();
+		if (secondCounter >= 1.0f) {
+			fpsText.setText("Frames per second: " + fps);
+			secondCounter = 0.0f;
+		}
+
+		// update states
+		stateManager.update(tpf);
+
+		// simple update and root node
+		debugger.reset();
+		simpleUpdate(tpf);
+		debugger.getNode();
+
+		rootNode.updateLogicalState(tpf);
+		guiNode.updateLogicalState(tpf);
+		rootNode.updateGeometricState();
+		guiNode.updateGeometricState();
+
+		// render states
+		stateManager.render(renderManager);
+		renderManager.render(tpf, true);
+		simpleRender(renderManager);
+		stateManager.postRender();
+	}
+
+
+	@Override
+	public void simpleUpdate(float tpf) {
+	}
+
+
+	@Override
+	public void simpleRender(RenderManager rm) {
+	}
+
+	public void simplePhysicsUpdate(float tpf) {
+	}
+
+	@Override
+	public void physicsTick(PhysicsSpace space, float f) {
+		simplePhysicsUpdate(f);
+	}
+
+	public PhysicsSpace getPhysicsSpace() {
+		return bulletAppState.getPhysicsSpace();
+	}
+
+	@Override
+	public void prePhysicsTick(PhysicsSpace arg0, float arg1) {
+	}
+
+	public void toggleToFullscreen() {
+		GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		DisplayMode[] modes = device.getDisplayModes();
+		int i = 0; // note: there are usually several, let's pick the first
+		settings.setResolution(modes[i].getWidth(), modes[i].getHeight());
+		settings.setFrequency(modes[i].getRefreshRate());
+		settings.setBitsPerPixel(modes[i].getBitDepth());
+		settings.setFullscreen(device.isFullScreenSupported());
+		appInstance.setSettings(settings);
+		appInstance.restart(); // restart the context to apply changes
+	}
+
+	public static void main(OpenRTSApplicationWithDI app) {
+		appInstance = app;
+		logger.info("seed : " + RandomUtil.SEED);
+
+		appInstance.start();
+	}
+
+	public void changeSettings() {
+		JmeSystem.showSettingsDialog(settings, false);
+		if (settings.isFullscreen()) {
+			logger.info("Fullscreen not yet supported");
+			settings.setFullscreen(false);
+		}
+
+		try {
+			settings.save("openrts.example");
+		} catch (BackingStoreException e) {
+			throw new TechnicalException(e);
+		}
+		appInstance.setSettings(settings);
+		appInstance.restart(); // restart the context to apply changes
+		cam.resize(settings.getWidth(), settings.getHeight(), true);
+	}
+
+	// public void startClient() {
+	// // ClientManager.startClient(stateManager, this);
+	// }
+
+	// public void stopClient() {
+	// ClientManager.stopClient();
+	// }
+
+}
