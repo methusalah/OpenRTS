e05edd149df06fba1ad4d153fe890abf0477f3b0
Mario Meltzow <github@meltzow.net>
2015-07-23 00:35:31 +0200

    strange TestNG was not found

--- server/test/java/openrts/server/ServerTest.java
+++ server/test/java/openrts/server/ServerTest.java
@@ -1,15 +1,12 @@
 package openrts.server;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.util.logging.Logger;
 
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+// import org.testng.Assert;
+// import org.testng.annotations.AfterClass;
+// import org.testng.annotations.AfterMethod;
+// import org.testng.annotations.BeforeClass;
+// import org.testng.annotations.BeforeMethod;
 
 /**
  * @author mario
@@ -73,62 +70,62 @@ public class ServerTest {
 	//
 	// }
 
-	private void waitUntilClientHasResponse(ClientEventListenerMock mock, int times) throws IOException {
-		int waitingCounter = times;
-		boolean waiting = true;
-		while (waiting) {
-			waitingCounter++;
-			if (mock.getEvent() != null) {
-				waiting = false;
-			} else {
-				logger.info("Waiting for answer from server...");
-			}
-			if (times > 0 && waitingCounter > 10) {
-				Assert.fail("Client are waiting too long..");
-			}
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-		}
-	}
+	// private void waitUntilClientHasResponse(ClientEventListenerMock mock, int times) throws IOException {
+	// int waitingCounter = times;
+	// boolean waiting = true;
+	// while (waiting) {
+	// waitingCounter++;
+	// if (mock.getEvent() != null) {
+	// waiting = false;
+	// } else {
+	// logger.info("Waiting for answer from server...");
+	// }
+	// if (times > 0 && waitingCounter > 10) {
+	// Assert.fail("Client are waiting too long..");
+	// }
+	// try {
+	// Thread.sleep(1000);
+	// } catch (InterruptedException ie) {
+	// ie.printStackTrace();
+	// }
+	// }
+	// }
+	//
+	// private void waitUntilServerIsStarted() throws IOException {
+	// boolean scanning = true;
+	// while (scanning) {
+	// Socket socket = new Socket();
+	// try {
+	// InetSocketAddress sa = new InetSocketAddress("localhost", OpenRTSServer.PORT);
+	//
+	// socket.connect(sa, 500);
+	// scanning = false;
+	// } catch (IOException e) {
+	// logger.severe("Connect failed, waiting and trying again");
+	// } finally {
+	// socket.close();
+	// }
+	// try {
+	// Thread.sleep(1000);
+	// } catch (InterruptedException ie) {
+	// ie.printStackTrace();
+	// }
+	// }
+	// }
+	//
+	// @BeforeClass
+	// public static void setUpClass() throws Exception {
+	// }
+	//
+	// @AfterClass
+	// public static void tearDownClass() throws Exception {
+	// }
 
-	private void waitUntilServerIsStarted() throws IOException {
-		boolean scanning = true;
-		while (scanning) {
-			Socket socket = new Socket();
-			try {
-				InetSocketAddress sa = new InetSocketAddress("localhost", OpenRTSServer.PORT);
+	// @BeforeMethod
+	// public void setUpMethod() throws Exception {
+	// }
 
-				socket.connect(sa, 500);
-				scanning = false;
-			} catch (IOException e) {
-				logger.severe("Connect failed, waiting and trying again");
-			} finally {
-				socket.close();
-			}
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-		}
-	}
-
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownClass() throws Exception {
-	}
-
-	@BeforeMethod
-	public void setUpMethod() throws Exception {
-	}
-
-	@AfterMethod
-	public void tearDownMethod() throws Exception {
-	}
+	// @AfterMethod
+	// public void tearDownMethod() throws Exception {
+	// }
 }
