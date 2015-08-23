e5893a00c0e89d5ec42d10b8d7d2bbf77b0bd2cf
Mario Meltzow <dev@meltzow.net>
2015-08-23 23:05:23 +0200

    fix imports

--- core/src/event/ClientLoggedOutEvent.java
+++ core/src/event/ClientLoggedOutEvent.java
@@ -1,9 +1,6 @@
 package event;
 
-import java.util.Date;
-
 import com.jme3.network.serializing.Serializable;
-import com.sun.xml.internal.ws.developer.StreamingAttachment;
 
 import event.network.NetworkEvent;
 
