40f0574b42eb186f691711a22f6774e8926577a9
eike <eike@hal>
2015-09-04 23:05:02 +0200

    FIX: Add missing class

---
+++ server/src/main/groovy/brainless/openrts/server/states/ServerLogicAppState.groovy
@@ -0,0 +1,41 @@
+package brainless.openrts.server.states
+
+import brainless.openrts.event.ClientLoggedOutEvent;
+import brainless.openrts.event.ClientTrysToLoginEvent;
+import brainless.openrts.event.EventManager
+import brainless.openrts.model.Player
+import brainless.openrts.server.ServerMain;
+
+import com.google.common.eventbus.Subscribe;
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class ServerLogicAppState extends AbstractAppState {
+	
+	 Map<Integer,String> loggedInPlayer
+	
+	ServerLogicAppState(ServerMain app){
+	}
+	
+	@Override
+	public void initialize(AppStateManager stateManager, Application app) {
+		super.initialize(stateManager, app);
+		EventManager.register(this);
+	}
+	
+	@Subscribe
+	def logSeverEvents(ClientTrysToLoginEvent evt) {
+		loggedInPlayer.put(evt.getId(), evt.getUser())
+	}
+	
+	@Subscribe
+	def logSeverEvents(ClientLoggedOutEvent evt) {
+		loggedInPlayer.remove(evt.getId())
+	}
+	
+
+}
